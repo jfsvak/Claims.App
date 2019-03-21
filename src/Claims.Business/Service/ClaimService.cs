494986162699c682cd4beb631bca0ea540e78cdb
Jesper Svak <svak@danskebank.com>
2019-03-21 14:26:02 +0800

    Refactor XmlParsing and add more test theory

--- src/Claims.Business/Service/ClaimService.cs
+++ src/Claims.Business/Service/ClaimService.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿using Claims.Business.Models;
+using Claims.Business.Util;
+using System;
+using System.Xml;
 
 namespace Claims.Business.Service
 {
     public class ClaimService
     {
+
+        public Claim ParseEmail(string email)
+        {
+            string textWithRootElement = string.Format("<root>{0}</root>", email);
+            TextToXmlParser p = new TextToXmlParser();
+            XmlDocument doc = p.ParseTextToXml(textWithRootElement);
+            
+            return new Claim();
+        }
 
     }
 }
